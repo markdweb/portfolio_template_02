5a80423c111bd22446a448ec5a5c574cf447f043
markdweb <markdhelweb1029@outlook.com>
2024-10-26 14:26:02 +0800

    upload template 02

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import WorkExp from "./components/WorkExp"
+import Education from "./components/Education"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <div className='relative h-full overflow-y-auto antialiased'>
+      <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
+        <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+            <Hero />
+            <Navbar />
+            <Projects />
+            <Skills />
+            <WorkExp />
+            <Education />
+            <Contact />
+            <Footer />
+        </div>
+    </div>
+  )
+}
+
+export default App
